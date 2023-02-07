0ecd8b6e6063b7ef89bcb3b4ccae27755d704334
Ayoola Marvellous <ayoolamarvellous@gmail.com>
2023-02-07 17:25:54 +0100

    Little Changes to the fragment_home.xml and adding the code to retrieve verses from the database to the HomeFragment.java

--- app/src/main/java/com/tmmarv/nlcf/Fragments/HomeFragment.java
+++ app/src/main/java/com/tmmarv/nlcf/Fragments/HomeFragment.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -11,9 +12,14 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
 import com.tmmarv.nlcf.R;
 import com.tmmarv.nlcf.ScheduleActivity;
 import com.tmmarv.nlcf.SermonsActivity;
@@ -23,20 +29,40 @@ public class HomeFragment extends Fragment {
     private Button calendarBtn;
     private Button sermonsBtn;
     private FirebaseAuth mAuth;
+    private TextView dailyVerse;
+    private TextView weeklyVerse;
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
         mAuth = FirebaseAuth.getInstance();
         View view = inflater.inflate(R.layout.fragment_home, container, false);
 
+        getDailyVerse();
+
         calendarBtn = view.findViewById(R.id.calendarBtn);
         sermonsBtn = view.findViewById(R.id.sermonBtn);
+        dailyVerse = view.findViewById(R.id.dailyVerseTv);
+        weeklyVerse = view.findViewById(R.id.weeklyVerseTv);
 
         calendarBtn.setOnClickListener(v -> startActivity(new Intent(getActivity(), ScheduleActivity.class)));
         sermonsBtn.setOnClickListener(v -> startActivity(new Intent(getActivity(), SermonsActivity.class)));
 
         return view;
     }
+
+    private void getDailyVerse() {
+        db.collection("daily").document(mAuth.getCurrentUser().getUid()).addSnapshotListener(new EventListener<DocumentSnapshot>() {
+            @Override
+            public void onEvent(@Nullable DocumentSnapshot value, @Nullable FirebaseFirestoreException error) {
+                assert value != null;
+                String verse = value.getString("daily");
+                dailyVerse.setText(verse);
+            }
+        });
+    }
+
 
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
